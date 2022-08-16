620c0dcf0c17ee48edf8e45b2b039cdedfe2c9ee
mihanyga <volk.mixail@gmail.com>
2022-08-16 15:15:24 +0400

    lesson_2: installed node.js, added react

---
+++ todo/frontend/src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const FooterItem = ({footer}) => {
+    return (
+        <div class='footer'>
+            <footer>
+                <p>ToDo Заметки. 2022 @ Комапания. Все права защищены.</p>
+            </footer>
+        </div>
+    )
+}
+
+export default FooterItem
